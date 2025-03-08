84516bada64fcfe2da0b1743894c468b99611a92
Mohammad Zeineddine <mohammad.zeineddine50@gmail.com>
2025-03-08 20:19:59 +0200

    Adding seed

--- Article-Server/database/users_migration.php
+++ Article-Server/database/users_migration.php
@@ -10,13 +10,4 @@
     }else{
         echo "USERS NOT Table Created";
     }
-
-    $query = $conn->prepare("CREATE TABLE IF NOT EXISTS questions (id AUTO_INCREMENT INT PRIMARY KEY,
-                                                                        question VARCHAR(255),
-                                                                        answer VARCHAR(255));");
-    if(sql_utils::query_execution($query,"",[])){
-        echo "QUESTIONS Table Created";
-    }else{
-        echo "QUESTIONS NOT Table Created";
-    }
 ?>
