d84fa399b6eb64b9d5764c684d9a9f1ae8175d79
Mohammad Zeineddine <mohammad.zeineddine50@gmail.com>
2025-03-09 00:10:37 +0200

    Adding addQuestion api

---
+++ Article-Server/api/v1/addQuestion.php
@@ -0,0 +1,28 @@
+<?php
+    $base = "../..";
+    require $base . "/connections/connection.php";
+
+    if ($_SERVER['CONTENT_TYPE'] === 'application/json') {
+        $data = json_decode(file_get_contents('php://input'), true);
+    } else {
+        $data = $_POST;
+    }
+
+    if(data_utils::missing_parm(2,$data, ["question","answer"])){
+        $question = $data["question"];
+        $answer = $data["answer"];
+        if(Question::create_question($question,$answer,$conn)){
+            echo json_encode(["result"=>true]);
+            echo json_encode(["message"=>"Question added successfully"]);
+            return;
+        } else{
+            echo json_encode(["result"=>false]);
+            echo json_encode(["message"=>"Question not added"]);
+            return;
+        }
+    } else{
+        echo json_encode(["result"=>false]);
+        echo json_encode(["message"=>"Both question and answer have to be filled"]);
+        return;
+    }
+?>
