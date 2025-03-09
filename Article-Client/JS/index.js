430cbcd103976948331f82ff1ccb92e7cfc4c85b
Mohammad Zeineddine <mohammad.zeineddine50@gmail.com>
2025-03-09 21:04:15 +0200

    editing base

--- Article-Client/JS/index.js
+++ Article-Client/JS/index.js
@@ -1,5 +1,5 @@
 const base = "../";
-const api_base = "http://localhost2/";
+const api_base = "http://13.38.107.39/Articles/";
 function alert_message(message){
     alert(message);
     return false;
